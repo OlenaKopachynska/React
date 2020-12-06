349c08d474e767e3668271cc38235e6d43fb749d
3048 <olena99y@gmail.com>
2020-12-06 14:30:59 +0200

    done

---
+++ src/database/users/users.js
@@ -0,0 +1,22 @@
+export  let array = [{
+        name: "sasha",
+        sumOfFields: 66,
+        sumO: "686868",
+        age: 44
+    },
+    {
+        breed: "british",
+        age: 9,
+        aaa: 5
+    }, {
+        breed: "british",
+        age: 9
+    }, {
+        breed: "british",
+        age: 9,
+        go: "rytty",
+    }, {
+        fff: "british",
+        age: 9
+    }
+];
